7e0c412ad1e1458e3ca4a9e3fd720f6c76f5c5c8
AJ Gebara <49355730+ajg7@users.noreply.github.com>
2021-03-23 20:24:40 -0500

    ran prettier

--- src/components/pages/Temperature.jsx
+++ src/components/pages/Temperature.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Temperature = () => {
-	return <div><h1>Temperature</h1></div>;
+	return (
+		<div>
+			<h1>Temperature</h1>
+		</div>
+	);
 };
 
 export default Temperature;
--- src/components/pages/UVIndex.jsx
+++ src/components/pages/UVIndex.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const UVIndex = () => {
-	return <div><h1>UV Index</h1></div>;
+	return (
+		<div>
+			<h1>UV Index</h1>
+		</div>
+	);
 };
 
 export default UVIndex;
