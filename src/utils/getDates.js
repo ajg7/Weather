13b84fc6f62fbb18e1250ed8e1ed303c18398356
AJ Gebara <49355730+ajg7@users.noreply.github.com>
2021-03-24 11:59:48 -0500

    made a helper function that gets the dates for the next 5 days

---
+++ src/utils/getDates.js
@@ -0,0 +1,11 @@
+export const getDates = () => {
+    const date = new Date();
+    const fiveDay = [];
+    let count = 0;
+    while (count < 5) {
+        fiveDay.push(`${date.getMonth() + 1}/${date.getDate() + count}`);
+        count++;
+    }
+
+    return fiveDay;
+};
