b79783dd0ccbee114d9972990ab11092ee670e3c
AJ Gebara <49355730+ajg7@users.noreply.github.com>
2021-03-24 14:30:48 -0500

    got rid of comments on useLineGraph

--- src/hooks/useLineGraph.js
+++ src/hooks/useLineGraph.js
@@ -2,9 +2,6 @@ import Chart from "chart.js";
 import { useEffect } from "react";
 
 export const useLineGraph = (chartRef, data, dataLabel, xLabels, units) => {
-	// This hook will set up the chart in the useEffect, and set the state of the data being passed down
-	// For example, it will take the temp data and then produce a chart with it, or it will take the wind data and make the data
-	// params: chartRef, data, typeOfGraph, dataLabel, labels
 	useEffect(() => {
 		const canvas = chartRef.current;
 		const ctx = canvas.getContext("2d");
