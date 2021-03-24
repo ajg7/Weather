31bd9cae7a569b4f62aa926ff5eb554662171ce5
AJ Gebara <49355730+ajg7@users.noreply.github.com>
2021-03-23 20:16:04 -0500

    set up basic rough draft of uvindex component and temperature component

---
+++ src/components/pages/Temperature.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Temperature = () => {
+	return <div><h1>Temperature</h1></div>;
+};
+
+export default Temperature;
---
+++ src/components/pages/UVIndex.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UVIndex = () => {
+	return <div><h1>UV Index</h1></div>;
+};
+
+export default UVIndex;
