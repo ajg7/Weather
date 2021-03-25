08b25e19e9b4ce9b540f1345c5879c355d442897
AJ Gebara <49355730+ajg7@users.noreply.github.com>
2021-03-25 18:50:11 -0500

    exported StyledWeatherChart and used useDarkMode in App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
-import { Reset } from "./styles/global";
+import { Reset, DarkMode } from "./styles/global";
 import { Route, Switch } from "react-router-dom";
 import { LandingPage } from "./components/pages";
+import { useDarkMode } from "./hooks";
 
 const App = () => {
+	const [darkMode, setDarkMode] = useDarkMode(false);
 	return (
 		<>
 			<Reset />
+			<DarkMode />
 			<Switch>
-				<Route path="/" component={LandingPage} />
+				<Route path="/">
+					<LandingPage darkMode={darkMode} setDarkMode={setDarkMode} />
+				</Route>
 			</Switch>
 		</>
 	);
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -2,5 +2,6 @@ import Descriptor from "./Descriptor";
 import Title from "./Title";
 import Button from "./Button";
 import WeatherChart from "./WeatherChart";
+import Toggle from "./Toggle";
 
-export { Descriptor, Title, Button, WeatherChart };
+export { Descriptor, Title, Button, WeatherChart, Toggle };
