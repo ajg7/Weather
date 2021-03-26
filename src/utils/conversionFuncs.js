0d4d19b1f609941b9a30dcf275df0fd88484e034
AJ Gebara <49355730+ajg7@users.noreply.github.com>
2021-03-25 21:51:50 -0500

    added what each conversion function returns in the comments

--- src/utils/conversionFuncs.js
+++ src/utils/conversionFuncs.js
@@ -1,17 +1,27 @@
 /**
- * Converts data from kelvin to celsius and returns the resulting array
+ * Converts data from kelvin to celsius
  * @param {number[]} data
+ * @return {number[]}
  */
 export const kelvinToCelsius = data => data.map(datum => (datum - 273.15).toFixed(2)); //Rounds numbers to 100s place
 
 /**
- * Converts data from celsius to kelvin and returns the resulting array
+ * Converts data from celsius to kelvin
  * @param {number[]} data
+ * @return {number[]}
  */
 export const kelvinToFahrenheit  = data => data.map(datum => (1.8 * (datum - 273) + 32).toFixed(2)); //Rounds numbers to 100s place
 
 /**
- * Converts data from celsius to fahrenheit and returns the resulting array
+ * Converts data from celsius to fahrenheit
  * @param {number[]} data
+ * @return {number[]}
  */
 export const celsiusToFahrenheit  = data => data.map(datum => (1.8 * datum + 32).toFixed(2)); //Rounds numbers to 100s place
+
+/**
+ * Converts data from meters per second to miles per hour
+ * @param {number[]} data
+ * @return {number[]}
+ */
+export const metersPerSecondToMPH = data => data.map(datum => ((datum * 3600) / 1609.34).toFixed(2));//Rounds numbers to 100s place
